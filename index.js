9704f921e909216e5f6c4c8e571c9f5841368940
Jamaludin Saputra <jamaludinsaputraaa@gmail.com>
2022-03-02 21:23:32 +0700

    revisi penggunaan grid[C

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-new gridjs.Grid({
-    columns: ["Nama", "Alamat", "Email", "No Tlp"],
+class Table {
+  constructor(init) {
+    this.init = init;
+  }
+
+  createHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.forEach((d) => {
+      open += `<th>${d}</th>`;
+    });
+
+    return open + close;
+  }
+
+  createBody(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
+
+    data.forEach((d) => {
+      open += `
+        <tr>
+          <td>${d[0]}</td>
+          <td>${d[1]}</td>
+          <td>${d[2]}</td>
+          <td>${d[3]}</td>
+        </tr>
+      `;
+    });
+
+    return open + close;
+  }
+
+  render(element) {
+    let table =
+      "<table class='table table-hover'>" +
+      this.createHeader(this.init.columns) +
+      this.createBody(this.init.data) +
+      "</table>";
+    element.innerHTML = table;
+  }
+}
+
+const table = new Table({
+  columns: ["Nama", "Alamat", "Email", "No Tlp"],
     data: [
       ["Embun", "Jakarta", "embun@gmail.com", "081377689954"],
-      ["Jamaludin", "Cilacap", "jamaludin@gmail.com", "087665887566"],
+      ["Jamaludin", "Cilacap", "jamaludin@gmail.com", "087665887566"]
     ]
-  }).render(document.getElementById("wrapper"));
+    });
+const app = document.getElementById("app");
+table.render(app);
